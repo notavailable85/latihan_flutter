231953ac823ad44b729101bbff8946295e63c237
Crash App <email.notavailable85@gmail.com>
2026-05-17 08:12:19 +0700

    latihan_column

--- lib/latihan_column.dart
+++ lib/latihan_column.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class LatihanColumn extends StatelessWidget {
   const LatihanColumn({super.key});
@@ -6,16 +7,57 @@ class LatihanColumn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Column")),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        // spacing: 50,
-        children: [
-          Text("Rania"),
-          Text("Selamat Ulang Tahun"),
-          Text("Mana Traktirannya?"),
-        ],
+      appBar: AppBar(
+        title: Text("Column", style: TextStyle(fontWeight: FontWeight.bold)),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 10,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 5,
+              children: [
+                Icon(Icons.man, size: 30),
+                Text("Man", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 5,
+              children: [
+                Icon(Icons.woman, size: 30),
+                Text("Woman", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 5,
+              children: [
+                Icon(Icons.boy, size: 30),
+                Text("Boy", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              spacing: 5,
+              children: [
+                Icon(Icons.girl, size: 30),
+                Text("Girl", style: TextStyle(fontSize: 16)),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
