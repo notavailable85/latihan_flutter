04250b92bcae0f39f71a3738e4cfaf77662c9a22
notavailable85 <email.notavailable85@gmail.com>
2026-05-20 07:44:47 +0700

    trial

---
+++ lib/trial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Trial extends StatefulWidget {
+  const Trial({super.key});
+
+  @override
+  State<Trial> createState() => _TrialState();
+}
+
+class _TrialState extends State<Trial> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
